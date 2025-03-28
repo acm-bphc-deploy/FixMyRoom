8ade6709038f1c4aab0021401266ec85b18257b3
sudheendrarao7 <ananicharitha@gmail.com>
2025-03-28 16:12:34 +0530

    after integrating database

--- src/supabaseClient.tsx
+++ src/supabaseClient.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "";  
-const SUPABASE_ANON_KEY = ""; 
+const SUPABASE_URL = "https://izzbmdswekuwqeshmgtr.supabase.co";  
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6emJtZHN3ZWt1d3Flc2htZ3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNTQxNzMsImV4cCI6MjA1ODczMDE3M30.2XqC0OlyIRj0ANBNuY-ZFVmYArC42FxljZiEowcK0yo"; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
