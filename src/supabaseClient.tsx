1c519da1a0ebf60a0d09815a6b44ae39d013ab0f
sudheendrarao7 <ananicharitha@gmail.com>
2025-07-01 23:37:50 +0530

    minor changes

--- src/supabaseClient.tsx
+++ src/supabaseClient.tsx
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://izzbmdswekuwqeshmgtr.supabase.co";  
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6emJtZHN3ZWt1d3Flc2htZ3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNTQxNzMsImV4cCI6MjA1ODczMDE3M30.2XqC0OlyIRj0ANBNuY-ZFVmYArC42FxljZiEowcK0yo"; 
-
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
