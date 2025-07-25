f9a398fb75a3920621a22035ad3a752f97d1b0da
unknown <ananicharitha@gmail.com>
2025-07-25 20:10:18 +0530

    debugging

--- src/supabaseClient.tsx
+++ src/supabaseClient.tsx
@@ -4,7 +4,5 @@ const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    flowType: 'pkce', 
-  },
+  
 });
